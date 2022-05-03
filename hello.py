e289207c23023d7894ffb68abf4a0e50d2022bde
pytech <onyangodonomondi@gmail.com>
2022-05-04 01:12:18 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+#Ask user for their name
+fName=input('What is your FirtsName?\n').strip().title()
+lName=input('What is your LastName?\n').strip().title()
+
+# say hello to user
+
+print('hello',fName, lName)
